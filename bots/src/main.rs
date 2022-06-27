b1a8ed24d902f6f2a1f8df116a1a4504c8d6279d
Mark Ribeiro <40504056+MarkRibeiro@users.noreply.github.com>
2022-06-27 04:01:12 -0300

    Permite que o jogo se atualize sem que o jogador se mova

--- bots/src/main.rs
+++ bots/src/main.rs
@@ -1,6 +1,14 @@
+use std::net::TcpStream;
 use rand::Rng;
+use std::io::{self, BufRead, BufReader, Write};
+use std::thread;
+use std::thread::Thread;
+use std::time::Duration;
+use tungstenite::{connect, Message, WebSocket};
+use tungstenite::stream::MaybeTlsStream;
 
 fn main() {
+    let id = 0;
     let mut number_of_bots = String::new();
     let mut port_number = String::new();
     println!("Quantos jogadores voce quer simular? ");
@@ -9,29 +17,42 @@ fn main() {
 
     println!("Qual a porta desejada? ");
     std::io::stdin().read_line(&mut port_number).unwrap();
-    let port_number = port_number.trim().parse::<i32>().unwrap();
 
-    while true {
-        next_moviment();
+    let (mut socket, response) =
+        //connect(format!("ws://127.0.0.1:{}", port_number.trim())).expect("Can't connect");
+        connect("ws://127.0.0.1:3012").expect("Can't connect");
+    socket.write_message(Message::Text(format!("conecta;{};yellow;0;0", id))).unwrap();
+
+
+    loop {
+        next_movement(&mut socket, id);
+        thread::sleep(Duration::from_millis(500));
     }
 }
 
-fn next_moviment() -> i32 {
+fn next_movement(socket: &mut WebSocket<MaybeTlsStream<TcpStream>>, id: i32) {
     let mut x = rand::thread_rng().gen_range(0..5);
-    if x == 0 {
-        println!("Cima");
+    let mut direction = "";
+    if x == 0 { //cima
+        println!("cima");
+        direction = "cima";
     }
-    if x == 1 {
-        println!("Baixo");
+    if x == 1 { //baixo
+        println!("baixo");
+        direction = "baixo";
     }
-    if x == 2 {
-        println!("Esquerda");
+    if x == 2 { //esquerda
+        println!("esquerda");
+        direction = "esquerda";
     }
-    if x == 3 {
-        println!("Direita");
+    if x == 3 { //direita
+        println!("direita");
+        direction = "direita";
     }
-    if x == 4 {
-        println!("Parado");
+    if x == 4 { //parado
+        socket.write_message(Message::Text(format!("pinta;{};0;0", id).into())).unwrap();
+        return;
     }
-    return x;
+    socket.write_message(Message::Text(format!("atualiza;{};{}", id, direction).into())).unwrap();
+    socket.write_message(Message::Text(format!("pinta;{};0;0", id).into())).unwrap();
 }
--- pf2/src/bin/main.rs
+++ pf2/src/bin/main.rs
@@ -1,14 +1,14 @@
 extern crate tungstenite;
 use pf2::ThreadPool;
-use std::fs;
+use std::{fs, io};
 use std::io::prelude::*;
 //use std::net::TcpListener;
 use std::net::TcpStream;
 use std::thread;
 use std::time::Duration;
 use std::{net::TcpListener, thread::spawn};
-use std::sync::{Arc, Mutex};
-use tungstenite::{accept, handshake::server::{Request, Response}, Message, WebSocket};
+use std::sync::{Arc, Mutex, MutexGuard};
+use tungstenite::{accept, Error, handshake::server::{Request, Response}, Message, WebSocket};
 
 struct Point {
   x:u32,
@@ -35,13 +35,39 @@ fn main() {
   let mut state = State{ players: vec![], map: vec![] };
   state.map = create_map();
   let current_state = Arc::new(Mutex::new(state));
-
+  let current_state_clone = current_state.clone();
+  let mut sockets:Vec<Arc<Mutex<WebSocket<TcpStream>>>> = Vec::new();
+  let mut sockets:Arc<Mutex<Vec<Arc<Mutex<WebSocket<TcpStream>>>>>> = Arc::new(Mutex::new(sockets));
+  let mut sockets_clone = sockets.clone();
+  pool.execute(move || {
+    loop {
+      //println!("oi");
+      let response;
+      thread::sleep(Duration::from_millis(9000000));
+      {
+        let mut state = current_state.clone();
+        response = build_response(false, &mut state.lock().unwrap());
+      }
+      let sockets = sockets_clone.clone();
+      let sockets = &*sockets.lock().unwrap();
+      for socket in sockets {
+        let mut socket = socket.lock().unwrap();
+        let _ = (*socket).write_message(Message::Text(response.clone()));
+      }
+    }
+  });
   for stream in listener.incoming() {
-    let mut websocket = accept(stream.unwrap()).unwrap();
+    let stream = stream.unwrap();
+    stream.set_nonblocking(true).unwrap();
+    let mut websocket = accept(stream).unwrap();
     let websocket =  Arc::new(Mutex::new(websocket));
-    let current_state = current_state.clone();
+    let current_state = current_state_clone.clone();
+    {
+      let mut sockets = sockets.clone();
+      let mut sockets = sockets.lock().unwrap();
+      sockets.push(websocket.clone());
+    }
     pool.execute(move || {
-
       handle_connection(websocket, current_state.clone());
     });
   }
@@ -95,13 +121,22 @@ fn handle_connection(mut stream: TcpStream) {
 fn handle_connection(websocket: Arc<Mutex<WebSocket<TcpStream>>>, current_state: Arc<Mutex<State>>){
   println!("Conectei");
   loop {
-    let msg = websocket.lock().unwrap().read_message().unwrap();
-    let copy_current_state = current_state.clone();
-    /*{
-        let mut state = current_state.lock().unwrap();
-        *state = msg.clone();
-    }*/
-    _process_message(websocket.clone(), msg, copy_current_state);
+    let msg = websocket.lock().unwrap().read_message();
+    match msg{
+      Ok(conteudo) => {
+        let copy_current_state = current_state.clone();
+        /*{
+            let mut state = current_state.lock().unwrap();
+            *state = msg.clone();
+        }*/
+        _process_message(websocket.clone(), conteudo, copy_current_state);
+      }
+      Err(Error::Io(ref e)) if e.kind() == io::ErrorKind::WouldBlock => {
+        //println!("WouldBlock");
+        continue;
+      }
+      Err(e) => panic!("encountered IO error: {e}")
+    }
   }
 }
 
@@ -118,7 +153,7 @@ fn _process_message(websocket: Arc<Mutex<WebSocket<TcpStream>>>, message:Message
     let jogador = Player{
       id: state.players.len(),
       color: info[2].to_string(),
-      posi: Point { x: 0, y: 0 },
+      posi: Point { x: 5, y: 5 },
       score: 0
     };
     state.players.push(jogador);
@@ -162,6 +197,14 @@ fn _process_message(websocket: Arc<Mutex<WebSocket<TcpStream>>>, message:Message
     }
   }
 
+  let ret = build_response(info[0] == "conecta", &mut state);
+
+  let _ = (*websocket).write_message(Message::Text(ret.clone()));
+  println!("{:?}", ret);
+
+}
+
+fn build_response(is_connected: bool, state: &mut MutexGuard<State>) -> String {
   let mut ret = String::from("{\"jogadores\" : [");
   let mut count = 0;
   for jogador in &state.players {
@@ -172,17 +215,14 @@ fn _process_message(websocket: Arc<Mutex<WebSocket<TcpStream>>>, message:Message
     if count < state.players.len() - 1 {
       ret.push(',');
     }
-    count = count +1
-
+    count = count + 1
   }
   ret = ret + &format!("], \"mapa\":{:?}", state.map);
 
-  if info[0]=="conecta" {
+  if is_connected {
     ret = ret + &format!(",\"id\": {}", state.players.len() - 1);
   }
 
   ret = ret + "}";
-  let _ = (*websocket).write_message(Message::Text(ret.clone()));
-  println!("{:?}", ret);
-
+  ret
 }
